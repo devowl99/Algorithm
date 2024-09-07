47b180a306a566ceeae040db7dd0c42f878f5836
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2024-09-07 19:22:47 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,21 +1,30 @@
 def solution(n, arr1, arr2):
-    answer = []
-    for i in range(n):
-        if len(str(bin(arr1[i]))[2:]) < n:
-            arr1[i] = '0'*(n-len(str(bin(arr1[i]))[2:])) + str(bin(arr1[i]))[2:]
-        else:
-            arr1[i] = str(bin(arr1[i]))[2:]
-        if len(str(bin(arr2[i]))[2:]) < n:
-            arr2[i] = '0'*(n-len(str(bin(arr2[i]))[2:])) + str(bin(arr2[i]))[2:]
-        else:
-            arr2[i] = str(bin(arr2[i]))[2:]
+    sol1 = toTwo(arr1, n)
+    sol2 = toTwo(arr2, n)
+    result= [""]*n
+
+    print(sol1)
+    print(sol2)
 
     for i in range(n):
-        a = ''
         for j in range(n):
-            if arr1[i][j] == '1' or arr2[i][j] == '1':
-                a = a + "#"
+            if(sol1[i][j]==1 or sol2[i][j]==1):
+                result[i]+="#"
             else:
-                a = a + " " 
-        answer.append(a)
-    return answer
+                result[i]+=" "
+
+    return result
+
+
+def toTwo(arr, n):
+    result = [[0 for i in range(n)] for j in range(n)]  # 2차원 배열 만들기
+
+    for i in range(len(arr)):  # 입력받은 값을 하나씩 꺼냄
+        flag = n - 1
+        while arr[i] > 0:  # arr[i]가 0이 될 때까지 반복
+            result[i][flag] = arr[i] % 2
+            arr[i] //= 2
+            flag -= 1
+        # arr[i]가 0일 때 추가 작업 필요 없음 (이미 0으로 초기화되어 있음)
+    
+    return result
