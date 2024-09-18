ee9a918454ed52a2e497f01a9ca001ffd9190f46
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2024-09-19 02:12:43 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,4 @@
 def solution(n):
-    binn = bin(n)[2:] # bin()
-    numone = binn.count('1')
-
     for i in range(n+1,1000001):
-        if bin(i)[2:].count('1') == numone:
+        if bin(n)[2:].count('1') == bin(i)[2:].count('1'):
             return i
