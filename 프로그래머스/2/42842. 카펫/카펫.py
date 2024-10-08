b250f57efcbe9994aec6ad6e07d5643bb5e28d34
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2024-10-08 14:26:59 +0900

    [level 2] Title: 카펫, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -7,10 +7,6 @@ def solution(brown, yellow):
         if sumAll % i == 0:
             if (i - 2) * (j - 2) == yellow:
                 if i > j:
-                    answer.append(i)
-                    answer.append(j)
+                    return [i, j]
                 else:
-                    answer.append(j)
-                    answer.append(i)
-
-                return answer
+                    return [j, i]
