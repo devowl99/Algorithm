eb9f6768ee052e860ef37db32fd51549193b12ad
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-02-18 20:53:35 +0900

    [level 2] Title: 주식가격, Time: 154.71 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,17 @@
 def solution(prices):
-    re = []
+    answer = []
+    pl = len(prices)
 
-    for i in range(len(prices)):
+    for i in range(pl):
+        if i == pl-1:
+            answer.append(0)
+            break
+
         count = 0
-        for j in range(i+1, len(prices)):
+        for j in range(i + 1, pl):
             count += 1
             if prices[i] > prices[j]:
                 break
-        re.append(count)
-    
-    return re
+        answer.append(count)
+
+    return answer
