7e55ae630536d0f42e8899768586335666146666
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-02-19 20:47:54 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,10 @@
 def solution(array, commands):
     answer = []
+
     for command in commands:
-        array2 = array[command[0]-1:command[1]:]
-        array2.sort()
-        answer.append(array2[command[2]-1])
+        slice = array[command[0]-1:command[1]:]
+        slice.sort()
+        
+        answer.append(slice[command[2]-1])
+
     return answer
