53771e57c12205552c15f89de54680b35eefba35
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-02-21 21:38:41 +0900

    [level 2] Title: 구명보트, Time: 13.47 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,13 @@
+from collections import deque
+
 def solution(people, limit):
-    people.sort()
+    d_p = deque(sorted(people))
     boat = 0
-    left = 0
-    right = len(people) - 1
-    
-    while left <= right:
-        if people[left] + people[right] <= limit:
-            left += 1
-        right -= 1
+
+    while d_p:
+        if len(d_p) > 1 and d_p[0] + d_p[-1] <= limit:
+            d_p.popleft()
+        d_p.pop()
         boat += 1
 
     return boat
