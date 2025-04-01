cb378919d056444d2b56d05eb8afcb5a266da82f
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-04-01 18:00:04 +0900

    [D3] Title: 식료품 가게, Time: 101 ms, Memory: 60,416 KB -BaekjoonHub

--- SWEA/D3/19113. 식료품 가게/식료품 가게.py
+++ SWEA/D3/19113. 식료품 가게/식료품 가게.py
@@ -6,9 +6,8 @@ for test_case in range(1, T + 1):
     discount = []
     used = []
     for P in Pi:
-        if P != 0:
-            if int(P*(4/3)) in Pi:
-                discount.append(P)
+        if P != 0 and int(P*(4/3)) in Pi:
+            discount.append(P)
             Pi[Pi.index(int(P*(4/3)))] = 0
 
     print(f'#{test_case}', *discount)
