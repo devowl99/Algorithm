b7185f4970150fa437ce05b197c51db8f35db7a1
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-05-04 03:21:28 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 4,458 ms, Memory: 74,368 KB -BaekjoonHub

---
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -0,0 +1,35 @@
+for test_case in range(1, 11):
+    tc = int(input())
+    board = [input() for _ in range(100)]
+
+    gotcha = 0
+    for p_len in range(100, 0, -1):
+        # 가로
+        for y in range(100):
+            for x in range(100-p_len+1):
+                word1 = board[y][x:x+p_len]
+                if word1 == word1[::-1]:
+                    gotcha = p_len
+                    break
+            if gotcha != 0:
+                break
+        if gotcha != 0:
+            break
+
+        # 세로
+        # row, col 위치 확인 잘하자
+        for col in range(100):
+            for row in range(100-p_len+1):
+                lst = []
+                for c in range(p_len):
+                    lst.append(board[row+c][col])
+                word2 = ''.join(lst)
+                if word2 == word2[::-1]:
+                    gotcha = p_len
+                    break
+            if gotcha != 0:
+                break
+        if gotcha != 0:
+            break
+
+    print(f'#{tc} {gotcha}')
