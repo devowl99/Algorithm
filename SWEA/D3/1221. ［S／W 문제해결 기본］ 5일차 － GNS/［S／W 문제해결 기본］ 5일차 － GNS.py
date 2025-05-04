9bbbad21b93009a85485560397a048ac723ab280
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-05-04 16:45:55 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 108 ms, Memory: 64,896 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -1,17 +1,19 @@
+from collections import Counter
+
 T = int(input())
 for test_case in range(1, T + 1):
-    trash, n = map(str, input().split())
-    nums = list(map(str, input().split()))
+    trash = input()
+    words = list(map(str, input().split()))
+    words_count = Counter(words)
+    answer0 = ['ZRO'] * words_count['ZRO']
+    answer1 = ['ONE'] * words_count['ONE']
+    answer2 = ['TWO'] * words_count['TWO']
+    answer3 = ['THR'] * words_count['THR']
+    answer4 = ['FOR'] * words_count['FOR']
+    answer5 = ['FIV'] * words_count['FIV']
+    answer6 = ['SIX'] * words_count['SIX']
+    answer7 = ['SVN'] * words_count['SVN']
+    answer8 = ['EGT'] * words_count['EGT']
+    answer9 = ['NIN'] * words_count['NIN']
 
-    answer0 = ' '.join(['ZRO'] * nums.count('ZRO'))
-    answer1 = ' '.join(['ONE'] * nums.count('ONE'))
-    answer2 = ' '.join(['TWO'] * nums.count('TWO'))
-    answer3 = ' '.join(['THR'] * nums.count('THR'))
-    answer4 = ' '.join(['FOR'] * nums.count('FOR'))
-    answer5 = ' '.join(['FIV'] * nums.count('FIV'))
-    answer6 = ' '.join(['SIX'] * nums.count('SIX'))
-    answer7 = ' '.join(['SVN'] * nums.count('SVN'))
-    answer8 = ' '.join(['EGT'] * nums.count('EGT'))
-    answer9 = ' '.join(['NIN'] * nums.count('NIN'))
-
-    print(f'#{test_case} {answer0} {answer1} {answer2} {answer3} {answer4} {answer5} {answer6} {answer7} {answer8} {answer9}')
+    print(f'#{test_case}', *answer0,*answer1,*answer2,*answer3,*answer4,*answer5,*answer6,*answer7,*answer8,*answer9)
