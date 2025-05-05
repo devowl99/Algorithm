a07695623b3eb77a431d55a54ec799c7c7716d37
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-05-05 13:49:26 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 55 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -1,59 +1,23 @@
-# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-# 아래 표준 입출력 예제 필요시 참고하세요.
+for test_case in range(1, 11):
+    n = int(input())
+    pw = list(map(int, input().split()))
+    cmd_num = int(input())
+    cmd = input().split()
 
-# 표준 입력 예제
-'''
-a = int(input())                        정수형 변수 1개 입력 받는 예제
-b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제 
-d = float(input())                      실수형 변수 1개 입력 받는 예제
-e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
-h = input()                             문자열 변수 1개 입력 받는 예제
-'''
+    i = 0
+    while i < len(cmd):
+        ist = []
+        if cmd[i] == 'I':
+            i += 1
+            idx = int(cmd[i])
+            i += 1
+            quan = int(cmd[i])
+            i += 1
+            for _ in range(quan):
+                ist.append(cmd[i])
+                i += 1
+                
+            pw[idx:idx] = list(map(int, ist))
 
-# 표준 출력 예제
-'''
-a, b = 6, 3
-c, d, e = 1.0, 2.5, 3.4
-f = "ABC"
-print(a)                                정수형 변수 1개 출력하는 예제
-print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
-print(c, d, e)                          실수형 변수 3개 출력하는 예제
-print(f)                                문자열 1개 출력하는 예제
-'''
-
-
-
-
-'''
-아래의 구문은 input.txt 를 read only 형식으로 연 후,
-앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
-아래 구문을 사용하기 위해서는 import sys가 필요합니다.
-단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
-'''
-#import sys
-#sys.stdin = open("input.txt", "r")
-
-from collections import deque
-
-T = 10
-for test_case in range(1, T + 1):
-    pw_len = int(input())
-    pw = input().split()
-    command_num = int(input())
-    command = input().split('I ')[1:] # 'I ' 앞 뒤로 나누기 때문에 I로 시작하면 앞에 공백이 생긴다.
-
-    for cmd in command:
-        cmd_list = deque(cmd.split())
-        location = cmd_list.popleft()
-        add_num = cmd_list.popleft()
-
-        cmd_list.reverse()
-        for i in cmd_list:
-            pw.insert(int(location), i) # 더 좋은 방법 : pw[x:x] = s(리스트)
-
-    str_pw = ' '.join(pw[:10])
-
-    print(f"#{test_case} {str_pw}")
+    answer = pw[:10]
+    print(f'#{test_case}', *answer)
