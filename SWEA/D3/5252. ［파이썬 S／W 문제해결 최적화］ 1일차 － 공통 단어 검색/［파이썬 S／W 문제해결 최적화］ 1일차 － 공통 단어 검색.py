185bb320ca9e93fb460eeb600fbda6d78fc07540
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-05-11 15:22:04 +0900

    [D3] Title: [파이썬 S/W 문제해결 최적화] 1일차 - 공통 단어 검색, Time: 181 ms, Memory: 64,512 KB -BaekjoonHub

--- SWEA/D3/5252. ［파이썬 S／W 문제해결 최적화］ 1일차 － 공통 단어 검색/［파이썬 S／W 문제해결 최적화］ 1일차 － 공통 단어 검색.py
+++ SWEA/D3/5252. ［파이썬 S／W 문제해결 최적화］ 1일차 － 공통 단어 검색/［파이썬 S／W 문제해결 최적화］ 1일차 － 공통 단어 검색.py
@@ -7,13 +7,10 @@ for test_case in range(1, T + 1):
         a.append(input())
     for _ in range(m):
         b.append(input())
-
+ 
     check = []
     for a_ in a:
         if a_ in b:
             check.append(a_)
-    for b_ in b:
-        if b_ in a:
-            check.append(b_)
-
+ 
     print(f'#{test_case} {len(list(set(check)))}')
