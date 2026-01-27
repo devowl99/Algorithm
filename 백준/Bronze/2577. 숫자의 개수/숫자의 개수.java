fc9c9ac2e2cfdab0d122656f019948c3dbd6b43b
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2026-01-27 21:38:10 +0900

    [Bronze II] Title: 숫자의 개수, Time: 64 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static long A, B, C;
+    static long total;
+    static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        A = Integer.parseInt(br.readLine());
+        B = Integer.parseInt(br.readLine());
+        C = Integer.parseInt(br.readLine());
+
+        total = A*B*C;
+        String s = String.valueOf(total);
+        numbers = new int[10];
+        for (int i=0; i<s.length(); i++) {
+            numbers[s.charAt(i)-'0'] += 1;
+        }
+
+        for (int i: numbers) {
+            System.out.println(i);
+        }
+    }
+}
