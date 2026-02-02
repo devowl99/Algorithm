1500ea23f4297c6ae88bdcb2067caad59ced13e7
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2026-02-02 23:01:42 +0900

    [Bronze I] Title: 더하기 사이클, Time: 60 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int origin;
+    static int n;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        origin = Integer.parseInt(br.readLine());
+        n = origin;
+
+        count = 0;
+        while (true){
+            count++;
+
+            int fir = n/10;
+            int sec = n%10;
+
+            n = n%10*10 + (fir+sec)%10;
+
+            if (n == origin) break;
+        }
+
+        System.out.println(count);
+    }
+}
