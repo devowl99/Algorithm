49af74166b01f8d6ffd3699421ce503308515a45
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2026-02-02 23:37:41 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        ans = 0;
+        for (int x = 1; x < N; x++) {
+            int sum = x;
+            int temp = x;
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            if (sum == N) {
+                ans = x;
+                break;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
